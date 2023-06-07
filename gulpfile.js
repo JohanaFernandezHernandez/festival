0933012ab231336cac2ff5c1b899d7359f3d210e
JohanaFernandezHernandez <najofe5@gmail.com>
2023-06-06 21:21:20 -0500

    imagenes, como hacer las imagenes menos pesadas con webp afit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const { src, dest, watch} = require ("gulp");
+const { src, dest, watch, parallel} = require ("gulp");
+
+//CSS
 const sass = require('gulp-sass')(require('sass'));
-const plumber =require('gulp-plumber')
+const plumber =require('gulp-plumber');
+
+
+//Imagenes
+const cache = require('gulp-cache');
+const imagemin = require ('gulp-imagemin');
+const webp = require ('gulp-webp');
+const avif = require ('gulp-avif');
 
 function css (done){
     src("src/scss/**/*.scss") //identificar el archivo de sass
@@ -11,10 +20,43 @@ function css (done){
     done();//avisa a Gulp cuando llega al final de la funcion
 }
 
+function imagenes (done) {
+    const opciones = {
+        optimizationLevel: 3 
+    }
+    src('src/img/**/*.{jpn,png}')
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest('build/img'))
+    done();
+}
+
+function versionWebp (done) {
+    const opciones = {
+        Quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+    .pipe(webp(opciones))
+    .pipe(dest('build/img'))
+    done();
+}
+
+function versionAvif (done) {
+    const opciones = {
+        Quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+    .pipe(avif (opciones))
+    .pipe(dest('build/img'))
+    done();
+}
+
 function dev(done) {
     watch("src/scss/**/*.scss", css)
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel (imagenes,versionAvif, versionWebp, dev);
